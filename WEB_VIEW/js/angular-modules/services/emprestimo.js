550acd9da799fb88ca91ef24f33329e90ab095f8
otaviomiranda <otaviomirand25@gmail.com>
2018-04-22 14:10:09 -0300

    Integração - Emprestimo

--- WEB_VIEW/js/angular-modules/services/emprestimo.js
+++ WEB_VIEW/js/angular-modules/services/emprestimo.js
@@ -3,5 +3,16 @@ const emprestimoSrv = angular.module("apacteca")
 emprestimoSrv.service("Emprestimo", ['$http',
     function ($http) {
 
+        this.getAll = (filter) => {
+            const request = {
+                obra_id : filter.idObra,
+                pessoa_id : filter.idPessoa,
+                data_emprestimo: filter.data_emprestimo,
+                data_devolucao: filter.data_devolucao
+            }
+            //console.log(request);
+            return $http.post(`http://localhost:8001/borrow/getAll`, request)
+        }
+        
     }
 ])
