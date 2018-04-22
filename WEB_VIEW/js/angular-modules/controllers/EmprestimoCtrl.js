b9275eea72a9e51e3a7a2348e88750e7a3f4bdee
otaviomiranda <otaviomirand25@gmail.com>
2018-04-22 14:09:43 -0300

    Integração - Emprestimo

--- WEB_VIEW/js/angular-modules/controllers/EmprestimoCtrl.js
+++ WEB_VIEW/js/angular-modules/controllers/EmprestimoCtrl.js
@@ -3,5 +3,44 @@ const EmprestimoCtrl = angular.module('apacteca')
 EmprestimoCtrl.controller('EmprestimoCtrl', ['$scope', 'Emprestimo', 'Notify',
     ($scope, Emprestimo, Notify) => {
 
+        $scope.emprestimo = {
+            // id_emprestimo : null,
+            idObra : null,
+            idPessoa: null,
+            nomePessoa : null,
+            titulo : null,
+            data_emprestimo: null,
+            data_devolucao: null
+        }
+
+        $scope.filter = {
+            idObra : null,
+            idPessoa : null,
+            titulo : null,
+            data_emprestimo: null,
+            data_devolucao: null
+        }
+
+        $scope.listaEmprestimo = [];
+
+        const OpenModalNovoEmprestimo = () => {
+
+        }
+
+        const getTodosEmprestimos = () => {
+            return Emprestimo.getAll($scope.filter)
+                .then((res) => {
+                    $scope.listaEmprestimo = res.data.data;
+                    console.log(res)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+
+
+        $scope.getTodosEmprestimos = getTodosEmprestimos
+
     }
 ])
+
