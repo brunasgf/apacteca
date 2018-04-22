f9e7c7818914e6cb590f0b5048d9f2ac98cfafb7
otaviomiranda <otaviomirand25@gmail.com>
2018-04-22 17:07:49 -0300

    Empréstimo - integração

--- WEB_VIEW/js/angular-modules/controllers/EmprestimoCtrl.js
+++ WEB_VIEW/js/angular-modules/controllers/EmprestimoCtrl.js
@@ -5,33 +5,54 @@ EmprestimoCtrl.controller('EmprestimoCtrl', ['$scope', 'Emprestimo', 'Notify',
 
         $scope.emprestimo = {
             // id_emprestimo : null,
-            idObra : null,
+            idObra: null,
             idPessoa: null,
-            nomePessoa : null,
-            titulo : null,
+            nomePessoa: null,
+            rgPessoa: null,
+            titulo: null,
             data_emprestimo: null,
             data_devolucao: null
         }
 
         $scope.filter = {
-            idObra : null,
-            idPessoa : null,
-            titulo : null,
+            idObra: null,
+            idPessoa: null,
+            titulo: null,
             data_emprestimo: null,
             data_devolucao: null
         }
 
         $scope.listaEmprestimo = [];
+        $scope.isDisabled = true
 
         const OpenModalNovoEmprestimo = () => {
-
+            return Notify.openModalTemplate("./views/emprestimo/criar.html")
+                .closePromise
+                .then(() => {
+                    getTodosEmprestimos()
+                })
+                .catch(() => {
+                    getTodosEmprestimos()
+                })
         }
+
+        const adicionarEmprestimo = (emprestimo) => {
+            return Emprestimo.create(emprestimo)
+                .then((res) => {
+                    console.log(res, "Criado com sucesso: EmprestimoCtrl")
+                    $scope.closeThisDialog()
+                })
+                .catch((err) => {
+                    console.log(err, "Algo não esta certo: EmprestimoCtrl")
+                })
+        }
+
 
         const getTodosEmprestimos = () => {
             return Emprestimo.getAll($scope.filter)
                 .then((res) => {
                     $scope.listaEmprestimo = res.data.data;
-                    console.log(res)
+                    console.log(emprestimo)
                 })
                 .catch((err) => {
                     console.log(err)
@@ -39,7 +60,20 @@ EmprestimoCtrl.controller('EmprestimoCtrl', ['$scope', 'Emprestimo', 'Notify',
         }
 
 
+        const getFormatedData = (data) => {
+            const date = new Date(parseInt(data));
+            const msg = date.toLocaleString();
+            if(msg == 'Invalid Date'){
+                return '-';
+            }else {
+                return msg;
+            }
+        }
+
+        $scope.getFormatedData = getFormatedData
+        $scope.adicionarEmprestimo = adicionarEmprestimo
         $scope.getTodosEmprestimos = getTodosEmprestimos
+        $scope.OpenModalNovoEmprestimo = OpenModalNovoEmprestimo
 
     }
 ])
--- WEB_VIEW/js/angular-modules/services/emprestimo.js
+++ WEB_VIEW/js/angular-modules/services/emprestimo.js
@@ -7,12 +7,25 @@ emprestimoSrv.service("Emprestimo", ['$http',
             const request = {
                 obra_id : filter.idObra,
                 pessoa_id : filter.idPessoa,
-                data_emprestimo: filter.data_emprestimo,
-                data_devolucao: filter.data_devolucao
+                data_emprestimo : filter.data_emprestimo,
+                data_devolucao : filter.data_devolucao
             }
             //console.log(request);
             return $http.post(`http://localhost:8001/borrow/getAll`, request)
         }
         
+        this.create = (emprestimo) => {
+            const request = {
+                idObra : emprestimo.idObra,
+                idPessoa : emprestimo.idPessoa,
+                nomePessoa : emprestimo.nomePessoa,
+                rgPessoa : emprestimo.rgPessoa,
+                
+            }
+
+            return $http.post(`http://localhost:8001/borrow`, request)
+        }
+
+       
     }
 ])
